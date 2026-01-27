ecccab0ffbcd449ae65ac1654f42399feb2ce461
Deanmark <deansy0718@gmail.com>
2026-01-27 19:54:01 +0800

    added more products

--- product.js
+++ product.js
@@ -1,4 +1,4 @@
-let products = [
+export let products = [
     {
         id: 1,
         image: 'https://www.sony.com.ph/image/6ff70f66ce935f02861635b3e936a70c?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF',
@@ -11,7 +11,7 @@ let products = [
         image: 'https://ph.garmin.com/m/ph/g/products/venu-x1-grey-cf-lg.jpg',
         name: 'Smart Watch Fitness Tracker',
         price: 129999,
-        about: 'True wireless earbuds with noise cancellaWaterproof smartwatch with heart rate monitor'
+        about: 'Waterproof smartwatch with heart rate monitor'
     },
     {
         id: 3,
@@ -69,5 +69,109 @@ let products = [
         price: 5999,
         about: 'UV-protection sunglasses with case'
     },
-    
+    {
+        id: 11,
+        image: 'https://gamextreme.ph/cdn/shop/products/1_83395474-e7b1-45a2-9ff1-cb73d2da03c0.png?v=1755160917',
+        name: 'Wireless Gaming Mouse',
+        price: 5499,
+        about: 'Ergonomic mouse with RGB lighting'
+    },
+    {
+        id: 12,
+        image: 'https://benson.ph/cdn/shop/products/ph-ur55-lu28r550uqexxp-frontblack-277485697.png?v=1670989616',
+        name: '4K Ultra HD Monitor',
+        price: 34999,
+        about: '32-inch 4K monitor with HDR support'
+    },
+    {
+        id: 13,
+        image: 'https://jblstore.com.ph/cdn/shop/files/JBLGo4_Main_Black_600x.png?v=1757250462',
+        name: 'Portable Bluetooth Speaker',
+        price: 6999,
+        about: 'Waterproof speaker with 12-hour battery'
+    },
+    {
+        id: 14,
+        image: 'https://merrell.com.ph/cdn/shop/files/MRLW-J068286-082823-F24-000.jpg?v=1756694834',
+        name: 'Running Shoes',
+        price: 7599,
+        about: 'Lightweight athletic shoes for running'
+    },
+    {
+        id: 15,
+        image: 'https://www.arkergonomics.com/cdn/shop/products/ark-chair-classic-720150.jpg?v=1693121148',
+        name: 'Ergonomic Office Chair',
+        price: 15999,
+        about: 'Adjustable chair with lumbar support'
+    },
+    {
+        id: 16,
+        image: 'https://hydroflask.ph/cdn/shop/files/1000504649_01_300x.jpg?v=1742819585',
+        name: 'Stainless Steel Water Bottle',
+        price: 2299,
+        about: 'Insulated bottle keeps drinks cold 24h'
+    },
+    {
+        id: 17,
+        image: 'https://jblstore.com.ph/cdn/shop/files/JBLTourOneM2_Black_1.png?v=1757250562',
+        name: 'Noise Cancelling Headphones',
+        price: 24999,
+        about: 'Premium over-ear headphones with ANC'
+    },
+    {
+        id: 18,
+        image: 'https://i5.walmartimages.com/seo/Ninja-4-Qt-Air-Fryer-Nonstick-Basket-with-Crisper-Plate-1550-Watts-Blak_0907e796-99b3-4021-938b-9a2a44a12ab2.13d1211bf2d0ac49e5bd757201e593c7.png',
+        name: 'Air Fryer',
+        price: 8999,
+        about: 'Digital air fryer with multiple presets'
+    },
+    {
+        id: 19,
+        image: 'https://img.lazcdn.com/g/p/29f21ec020168c0c88470113bd144ecd.jpg_720x720q80.jpg',
+        name: 'Yoga Mat',
+        price: 2999,
+        about: 'Non-slip eco-friendly yoga mat'
+    },
+    {
+        id: 20,
+        image: 'https://m.media-amazon.com/images/I/71kRYvbacuL._AC_UF894,1000_QL80_.jpg',
+        name: 'Smart Home Security Camera',
+        price: 4999,
+        about: 'WiFi camera with night vision'
+    },
+    {
+        id: 21,
+        image: 'https://static.beautytocare.com/cdn-cgi/image/f=auto/media/catalog/product//o/r/oral-b-vitality-pro-protect-x-clean-electric-toothbrush-black.jpg',
+        name: 'Electric Toothbrush',
+        price: 4299,
+        about: 'Sonic toothbrush with 4 brush heads'
+    },
+    {
+        id: 22,
+        image: 'https://stancephilippines.com/cdn/shop/products/Portable-Adjustable-Laptop-Stand_1024x1024.jpg?v=1675308912',
+        name: 'Laptop Stand',
+        price: 3499,
+        about: 'Adjustable aluminum laptop stand'
+    },
+    {
+        id: 23,
+        image: 'https://m.media-amazon.com/images/I/71L705RjIML._AC_UF1000,1000_QL80_.jpg',
+        name: 'Car Phone Holder',
+        price: 1499,
+        about: 'Magnetic car mount for smartphones'
+    },
+    {
+        id: 24,
+        image: 'https://mobileimages.lowes.com/productimages/b8e6068d-58a3-4076-9328-c57f25de82e5/67572327.jpeg?size=pdhz',
+        name: 'Desk Lamp',
+        price: 3299,
+        about: 'LED desk lamp with adjustable brightness'
+    },
+    {
+        id: 25,
+        image: 'https://dynaquestpc.com/cdn/shop/products/UGREENFASTCHARGERADAPTERwPD20WWHITECD137-60449.jpg?v=1645521130&width=500',
+        name: 'Fast Charging Adapter',
+        price: 2799,
+        about: '65W GaN charger with 3 USB ports'
+    }
 ]
